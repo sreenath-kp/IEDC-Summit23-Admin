40f30b3f5c3f38b9f91c16c692a2e345a781abbb
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-02 17:05:36 +0530

    ref in attendecontroller provider

--- lib/controller/attendee_controller.dart
+++ lib/controller/attendee_controller.dart
@@ -11,17 +11,19 @@ final getAttendeeByIDProvider = StreamProvider.family(
 final attendeeControllerProvider =
     StateNotifierProvider<AttendeeController, bool>((ref) {
   return AttendeeController(
-      attendeeRepository: ref.watch(attendeeRepositoryProvider), ref: ref);
+    attendeeRepository: ref.watch(attendeeRepositoryProvider),
+    // ref: ref,
+  );
 });
 
 class AttendeeController extends StateNotifier<bool> {
   final AttendeeRepository _attendeeRepository;
-  final Ref _ref;
+  // final Ref _ref;
   AttendeeController({
     required AttendeeRepository attendeeRepository,
-    required Ref ref,
+    // required Ref ref,
   })  : _attendeeRepository = attendeeRepository,
-        _ref = ref,
+        // _ref = ref,
         super(false);
 
   Stream<Attendee> getDatabyId(String id) {
