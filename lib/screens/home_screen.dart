2ac02ebbf4073d007be3c4f8bcaa0a1e1c83edea
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-04 21:09:18 +0530

    minor changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -97,16 +97,16 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
               title: "Workshops",
               func: () {},
             ),
-            // const SizedBox(
-            //   height: 50,
-            // ),
-            HomeButton(
-              title: "Upload firebase",
-              func: () {
-                TownscriptRepository(firestore: ref.watch(firestoreProvider))
-                    .gettingData();
-              },
+            const SizedBox(
+              height: 50,
             ),
+            // HomeButton(
+            //   title: "Upload firebase",
+            //   func: () {
+            //     TownscriptRepository(firestore: ref.watch(firestoreProvider))
+            //         .gettingData();
+            //   },
+            // ),
           ],
         ),
       ),
