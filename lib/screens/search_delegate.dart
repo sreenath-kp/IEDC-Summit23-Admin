a1187b72a4c93932ff823a72dd9c9504c916bec4
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-10-10 17:14:41 +0530

    the search delegate is set

--- lib/screens/search_delegate.dart
+++ lib/screens/search_delegate.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:summit_admin_app/components/progress_indicator.dart';
+import 'package:summit_admin_app/controller/workshop_controller.dart';
+import 'package:summit_admin_app/screens/workshop_scanner.dart';
 
-class SearchWorkShops extends SearchDelegate {
+class SearchWorkShopsDelegate extends SearchDelegate {
+  final WidgetRef _ref;
+  SearchWorkShopsDelegate({
+    required WidgetRef ref,
+  }) : _ref = ref;
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -25,7 +34,27 @@ class SearchWorkShops extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    return _ref.watch(getWorkShopsByNameProvider(query)).when(
+          data: (workshops) => ListView.builder(
+            itemBuilder: (context, index) {
+              final workshop = workshops[index];
+              return ListTile(
+                  title: Text(workshop.title),
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            WorkshopScannerScreen(wsName: workshop.title),
+                      ),
+                    );
+                  });
+            },
+            itemCount: workshops.length,
+          ),
+          error: (error, stackTrace) {
+            return const Text("error");
+          },
+          loading: () => const Loader(),
+        );
   }
 }
