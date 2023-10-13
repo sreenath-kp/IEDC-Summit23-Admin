e694ff67d889076f4c56fe7a4ce8965d97750c74
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2023-10-14 01:29:45 +0530

    web

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -17,10 +17,7 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      throw UnsupportedError(
-        'DefaultFirebaseOptions have not been configured for web - '
-        'you can reconfigure this by running the FlutterFire CLI again.',
-      );
+      return web;
     }
     switch (defaultTargetPlatform) {
       case TargetPlatform.android:
@@ -48,6 +45,16 @@ class DefaultFirebaseOptions {
         );
     }
   }
+
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyCJyfqomITmmeZX9BDg-evO0P7AapsLRno',
+    appId: '1:671098905742:web:b1580af56e2cfb850955df',
+    messagingSenderId: '671098905742',
+    projectId: 'iedc-pro-app',
+    authDomain: 'iedc-pro-app.firebaseapp.com',
+    storageBucket: 'iedc-pro-app.appspot.com',
+    measurementId: 'G-HVTJ93SRVK',
+  );
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyA4bJ8fNsjtOPbv9zyIV1FvwddKFdCzHdw',
